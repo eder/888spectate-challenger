b42d8e98fc7e26fbb9c5701cfeb7231ff392f02f
Eder Eduardo <eder.esilva@gmail.com>
2023-10-02 21:56:27 +0100

    -  for trees with no parents.

--- code-challenger/find_internal_nodes.py
+++ code-challenger/find_internal_nodes.py
@@ -1,14 +1,30 @@
 from typing import List
 
 def find_internal_nodes_num(tree: List[int]) -> int:
-   
+    """
+    Function to find the number of internal nodes in a tree representation.
+    The tree is represented as a list of integers where each value is an index pointing to its parent.
+    The root node (or nodes without a parent) are represented by a value of -1.
+    Args:
+    - tree (List[int]): A list representing the tree structure.
+    Returns:
+    - int: The number of internal nodes (unique parents) in the tree.
+    """
+    
+    # Initialize an empty dictionary to store node counts
     count = {}
+
+    # Iterate through each node in the tree
     for node in tree:
+        # Check if the current node is not a root (or a node without a parent)
         if node != -1:
+            # If the node is already in the count dictionary, increment its count
             if node in count:
                 count[node] += 1
+            # Otherwise, add the node to the dictionary with a count of 1
             else:
                 count[node] = 1
 
+    # Return the number of unique parent nodes (internal nodes) in the tree
     return len(count)
 
--- code-challenger/test_find_internal_nodes.py
+++ code-challenger/test_find_internal_nodes.py
@@ -3,8 +3,23 @@ from find_internal_nodes import find_internal_nodes_num
 
 class TestClass:
     def test_validate_non_empty_list_input(self):
-       def test_find_internal_nodes_1(self):
+        # Check if input is a list
+        my_tree = [4, 4, 1, 5, -1, 4, 5]
+        assert isinstance(my_tree, list), "Input should be a list"
+
+        # Check if list is not empty
+        assert len(my_tree) > 0, "Input list should not be empty"
+        
+        # Now you can also check the functionality if you want
+        assert find_internal_nodes_num(my_tree) == 3
+
+    def test_find_internal_nodes_1(self):
         my_tree = [4, 4, 1, 5, -1, 4, 5]
         assert find_internal_nodes_num(my_tree) == 3
+
+    def test_find_internal_nodes_2(self):
+        # A tree with no parent nodes (all values are -1)
+        my_tree = [-1, -1, -1, -1]
+        assert find_internal_nodes_num(my_tree) == 0
 
 
