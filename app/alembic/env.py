8278c18f5cc352016182a0e4ab296f728e315fc4
Eder Eduardo <eder.esilva@gmail.com>
2023-10-04 02:20:37 +0100

    Removed ORM (My bad)

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -1,24 +1,56 @@
+from __future__ import with_statement
 import os
+from logging.config import fileConfig
 
-from gino import Gino
-from db.models import db  
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+
+from alembic import context
+
 DATABASE_URL = os.environ['DATABASE_URL'] 
 
-def run_migrations_offline():
-    # ...
-    context.configure(
-        url=DATABASE_URL,
-        target_metadata=db,
-        literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
-    )
-    # ...
+
+from sqlalchemy import MetaData
+metadata = MetaData()
+
+config = context.config
+
+fileConfig(config.config_file_name)
+
+DATABASE_URL = os.environ.get('DATABASE_URL') or config.get_main_option("sqlalchemy.url")
+
+config.set_main_option('sqlalchemy.url', DATABASE_URL)
 
 def run_migrations_online():
-    # ...
+    """Run migrations in 'online' mode."""
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix='sqlalchemy.',
+        poolclass=pool.NullPool,
+    )
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=metadata,
+            render_as_batch=True 
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+def run_migrations_offline():
+    """Run migrations in 'offline' mode."""
     context.configure(
         url=DATABASE_URL,
-        target_metadata=db
+        target_metadata=metadata,
+        render_as_batch=True  
     )
-    # ...
 
+    with context.begin_transaction():
+        context.run_migrations()
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
